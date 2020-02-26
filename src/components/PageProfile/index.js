8b9358618aebd65e5891946bee6c8568cdbbf4bd
VovaSidorov <VovaCivic@gmail.com>
2020-02-26 16:27:20 +0200

    Add components to posts

--- src/components/PageProfile/index.js
+++ src/components/PageProfile/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import s from './style.module.css';
+import MyPosts from "./MyPosts";
 
 const PageProfile = () => {
     return (
@@ -8,11 +9,7 @@ const PageProfile = () => {
                 <img src='https://linguist.lviv.ua/wp-content/uploads/2015/02/image.png' alt="sdfdsfs"/>
             </div>
                 <div>AVA</div>
-                <div>My Post</div>
-                <div>New post</div>
-                <div>Post 1</div>
-                <div>Post 2</div>
-                <div>Post 3</div>
+                <MyPosts/>
         </div>
     );
 };
