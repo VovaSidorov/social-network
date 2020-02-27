27f666d9d02a708a1de8cac7d0dc13df38232050
VovaSidorov <VovaCivic@gmail.com>
2020-02-27 14:18:14 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import PageProfile from "./components/PageProfile";
 import Dialog from "./components/Dialog";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
     return (
+        <BrowserRouter>
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
-            {/*<PageProfile/>*/}
+           
             <div className='app-wrapper-content'>
-                <Dialog/>
+                <Route path="/profile" component={PageProfile}/>
+                <Route path="/messages" component={Dialog}/>
+            
             </div>
         </div>
+        </BrowserRouter>
 
     );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,10 +5,10 @@ const Navbar = () => {
     return (
         <nav className={s.sidebar}>
             <div>
-                <a href="#1">Profile</a>
+                <a href="/profile">Profile</a>
             </div>
             <div>
-                <a href="#2">Messages</a>
+                <a href="/messages">Messages</a>
             </div>
             <div>
                 <a href="#3">News</a>
