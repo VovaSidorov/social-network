d60302169303d084d55150648cfd46d8226d9b37
VovaSidorov <VovaCivic@gmail.com>
2020-02-27 21:23:07 +0200

    add naw link add add class active to links

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,22 +1,23 @@
 import React from "react";
 import s from './style.module.css';
+import {NavLink} from "react-router-dom"
 
 const Navbar = () => {
     return (
         <nav className={s.sidebar}>
-            <div>
-                <a href="/profile">Profile</a>
+            <div className={s.item}>
+                <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink>
             </div>
-            <div>
-                <a href="/messages">Messages</a>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to="/messages" activeClassName={s.activeLink}>Messages</NavLink>
             </div>
-            <div>
+            <div className={s.item}>
                 <a href="#3">News</a>
             </div>
-            <div>
+            <div className={s.item}>
                 <a href="#4">Musik</a>
             </div>
-            <div>
+            <div className={s.item}>
                 <a href="#5">Settings</a>
             </div>
         </nav>
