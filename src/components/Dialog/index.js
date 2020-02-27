31da9485e485cdd362278e93fca51c7cde2f08fa
VovaSidorov <VovaCivic@gmail.com>
2020-02-27 22:04:42 +0200

    some refractor component Dialog

--- src/components/Dialog/index.js
+++ src/components/Dialog/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import s from './style.module.css';
 
-const Dialog= ()=>{
+const Dialog = () => {
     return (
-        <div>
-            Dialog
-        </div>
+
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={s.dialog + ' '+ s.active}>
+                        Vova
+                    </div>
+                    <div className={s.dialog}>
+                        Sveta
+                    </div>
+                    <div className={s.dialog}>
+                        Andrey
+                    </div>
+                    <div className={s.dialog}>
+                        Dima
+                    </div>
+                </div>
+                <div className={s.messages}>
+                    <div className={s.message}>Hi</div>
+                    <div className={s.message}>How is your health?</div>
+                    <div className={s.message}>yo</div>
+                    <div className={s.message}>Lorem Ipsum</div>
+                </div>
+            </div>
+
+
+
+
     )
 
 };
