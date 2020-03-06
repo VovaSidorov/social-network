9d453e84a82331e7149c7d521ed4ed2f0d95925a
VovaSidorov <VovaCivic@gmail.com>
2020-03-07 00:34:01 +0200

    add reducer

--- src/components/Dialog/index.js
+++ src/components/Dialog/index.js
@@ -5,7 +5,7 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import {makeStyles} from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-import {updateNewMessageBodyCreator,sendMessageCreator} from './../../redux/state';
+import {updateNewMessageBodyCreator,sendMessageCreator} from './../../redux/dialogsReducer';
 
 const useStyles = makeStyles(theme => ({
     paper: {
