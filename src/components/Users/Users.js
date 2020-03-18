d95e8087c00d5f878bef8ddebcb23a1efa65588c
VovaSidorov <VovaCivic@gmail.com>
2020-03-18 22:58:46 +0200

    follow-unfollow api

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -2,6 +2,7 @@ import React from 'react';
 import userPhoto from '../../essets/images/user.png';
 import styles from './style.module.css';
 import {NavLink} from "react-router-dom";
+import * as axios from "axios";
 
 
 
@@ -32,10 +33,32 @@ return (
                 <div>
                     {u.followed
                         ? <button onClick={() => {
-                            props.unfollow(u.id)
+                            axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                                withCredentials: true,
+                                headers: {
+                                    "API-KEY": "44710690-cea3-4aa6-b22a-5616fcf5eb47"
+                                }
+                            })
+                                .then(response => {
+                                    if (response.data.resultCode == 0) {
+                                        props.unfollow(u.id);
+                                    }
+                                });
+
                         }}>Unfollow</button>
+
                         : <button onClick={() => {
-                            props.follow(u.id)
+                            axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                withCredentials: true,
+                                headers: {
+                                    "API-KEY": "44710690-cea3-4aa6-b22a-5616fcf5eb47"
+                                }
+                            })
+                                .then(response => {
+                                    if (response.data.resultCode==0){
+                                        props.follow(u.id);
+                                    }
+                                });
                         }}>Follow</button>}
 
                 </div>
